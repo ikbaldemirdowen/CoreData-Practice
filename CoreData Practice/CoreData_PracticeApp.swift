201f3b8c8bf49da28a00fb2ef6a089d5a289fedf
Ikbal Demirdoven <ikbaldemirdoven@icloud.com>
2023-05-24 22:22:45 -0700

    Initial Commit

---
+++ CoreData Practice/CoreData_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreData_PracticeApp.swift
+//  CoreData Practice
+//
+//  Created by Ikbal Demirdoven on 2023-05-24.
+//
+
+import SwiftUI
+
+@main
+struct CoreData_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
